a2ce34c42fc051282fb2afb912ab7453ac292110
Sukma Anggarmadi <2111527001_sukma@student.unand.ac.id>
2023-06-29 00:12:38 +0700

    multer

--- src/index.js
+++ src/index.js
@@ -59,6 +59,50 @@ app.use("/auth", authRouter);
 //   });
 // });
 
+app.get("/download/:fileName", authenticateToken, function (req, res) {
+  const fileName = req.params.fileName;
+  const filePath = __dirname + "/assets/files_upload/" + fileName;
+
+  console.log(filePath);
+
+  res.download(filePath, function (err) {
+    if (err) {
+      // Tangani kesalahan jika terjadi
+      console.log(err);
+      res.status(404).send("File tidak ditemukan");
+    }
+  });
+});
+
+// Definisikan endpoint untuk mengirimkan gambar avatar
+app.get("/avatar", async (req, res) => {
+  const user_id = req.session.user_id;
+  // Ambil path file gambar avatar dari database
+  // const avatarPath = getGambarPath(user_id); // Ganti dengan path yang sesuai di database
+  // console.log("Ada? : ", avatarPath);
+
+  const gambar = await User.findOne({ where: { user_id: user_id } });
+
+  // Periksa apakah gambar ditemukan
+  if (!gambar) {
+    throw new Error("Gambar tidak ditemukan");
+  }
+
+  // Ambil path gambar dari objek Sequelize
+  const imagePath = gambar.avatar;
+
+  // Periksa apakah file gambar ada
+  if (fs.existsSync(imagePath)) {
+    // Set header tipe konten sebagai gambar
+    res.setHeader("Content-Type", "image/jpeg"); // Ganti dengan tipe konten yang sesuai
+
+    // Baca file gambar dan kirimkan sebagai respons
+    fs.createReadStream(imagePath).pipe(res);
+  } else {
+    res.status(404).send("Gambar avatar tidak ditemukan");
+  }
+});
+
 app.use((err, req, res, next) => {
   res.json({
     message: err.message,
